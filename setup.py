603ed85c26e8f1a651fd79ba3a7c06b807472a22
alexsilva <geniofuturo@gmail.com>
2022-12-07 15:48:55 -0300

    Set django>=3,<4.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/moggers87/django-sendfile2',
     license='BSD',
 
-    install_requires=['django'],
+    install_requires=['django>=3,<4'],
     packages=['django_sendfile', 'django_sendfile.backends'],
     extras_require={
         "docs": [
